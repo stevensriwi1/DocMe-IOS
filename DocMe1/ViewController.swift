48c115c19728bbedce368b59512a36b87d51326c
Steven <steven_sriwi@hotmail.com>
2019-12-27 11:22:24 +1100

    Initial Commit

---
+++ DocMe1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocMe1
+//
+//  Created by Rachel Su Jia Xin on 27/12/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
